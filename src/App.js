5aa854c1c927d060c84f8468d112f5811229c970
Mike <ml.fidanyan@gmail.com>
2022-09-20 20:50:08 +0000

    just some more hook play around

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Counterfunction from './components/reacthook.js'
 function App() {
   return (
     <div className="App">
-      <h1>This has been coded with an Ipad </h1>
+      
       <Counterfunction/>
     </div>
   );
--- src/components/reacthook.js
+++ src/components/reacthook.js
@@ -1,16 +1,15 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 
 export default function Counterfunction(){
   
- const [count, setCount] = useState(0);
- 
- 
+ const [count, setCount] = useState(0)
    return(
      <div>
      
-     <p>You have clicked me {count} times</p>
-     <button onClick={()=>setCount(count+1)} >Click me</button>
+   <p>You have clicked me {count} times</p>
+     <button onClick={()=>setCount(count+1)}>
+     Click me</button>
  
      </div>
    
