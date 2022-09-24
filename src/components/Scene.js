85054fb94e1e246f503c23d53300ce69eca4bf0e
Mike <ml.fidanyan@gmail.com>
2022-09-24 12:01:49 +0000

    Update 3 files

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,9 +1,28 @@
-import Spline from '@splinetool/react-spline';
+import Spline from "@splinetool/react-spline";
+import { useRef } from "react";
 
-export default function Scene(){
-  return(
-    <div className='scene-container'>
-      <Spline scene="https://prod.spline.design/9PMQrISi0ljggzzI/scene.splinecode"/>
+export default function Scene() {
+  const scene = useRef();
+
+  function onLoad(spline) {
+    const obj = spline.findObjectById("976210e3-1a76-4d88-bef1-dc5c3e9e80e1");
+    scene.current = obj;
+  }
+
+  function moveObj() {
+    console.log(scene.current);
+    scene.current.rotation.y += 80;
+  }
+
+  return (
+    <div className="scene-container">
+      <Spline
+        scene="https://prod.spline.design/9PMQrISi0ljggzzI/scene.splinecode"
+        onLoad={onLoad}
+      />
+      <button style={{ position: "absolute" }} type="button" onClick={moveObj}>
+        Move Object
+      </button>
     </div>
   );
 }
