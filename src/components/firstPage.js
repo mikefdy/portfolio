7bdbb2e9956e1200a6f80a921a315ac0e4421abe
Mike <ml.fidanyan@gmail.com>
2022-10-02 15:06:19 +0000

    Update App.css and firstPage.js final draft of firstPage.js

--- src/components/firstPage.js
+++ src/components/firstPage.js
@@ -5,20 +5,23 @@ import MyCodeBlock from "./codeblock.js";
 
 export default function Home() {
   const obj =
-    "{ return (\n<div className='container'>\n  <code>\n    <h2>Welcome!</h2>\n    <p>Thanks for joining.\n   Keep on scrolling to get to know me better\n    <p>\n  </code>\n</div>);\n}";
+    " return (\n<div className='container'>\n  <code>\n    <h2>Welcome!</h2>\n    <p>Thanks for joining.\n   Keep on scrolling to get to know me better\n    <p>\n  </code>\n</div>\n   );\n   }\n";
+  const name = "Mike Fidanyan(){";
   return (
     <div className="home_container">
+      <h1 style={{ color: "white" }}>export default function</h1>
+
       <MovingComponent
         type="fadeInFromBottom"
-        duration="1000ms"
+        duration="5000ms"
         delay="0s"
         direction="normal"
         timing="ease"
         iteration="1"
         fillMode="none"
       >
-        <div className="name-card">
-          <h1 className="name">Mike Fidanyan</h1>
+        <div className="name">
+          <h1>{name}</h1>
         </div>
       </MovingComponent>
       <div className="codeblock">
